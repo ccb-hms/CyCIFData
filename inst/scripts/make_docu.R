b8bd69ee4615ff88b995de5cd4cacffc721d3126
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-09-14 14:12:17 -0400

    update doc data.frame

--- inst/scripts/make_docu.R
+++ inst/scripts/make_docu.R
@@ -3,14 +3,18 @@ df <- data.frame(
     DataProvider = "Laboratory of Systems Pharmacology at Harvard Medical School",
     TaxonomyId = "9606",
     Species = "Homo sapiens",
+    Title = "Exemplar Microscopy Images of Tissues (EMIT)",
     SourceUrl = "https://www.synapse.org/#!Synapse:syn22345748/wiki/",
     SourceType = "TXT",
+    Location_Prefix = "https://mghp.osn.xsede.org/",
+    RDataPath = "bir190004-bucket01/TMA11/zarr/",
+    RDataClass = "ZarrRemote",
     SourceVersion = "1.0.0",
     Maintainer  = "Ludwig Geistlinger <ludwig_geistlinger@hms.harvard.edu>",
     stringsAsFactors = FALSE
 )
 write.csv(
     df,
-    file = "inst/extdata/docuData/CyCIFData_v1.csv",
+    file = "~/gh/CyCIFData/inst/extdata/docuData/CyCIFData_v1.csv",
     row.names = FALSE
 )
