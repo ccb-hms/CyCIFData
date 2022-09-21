359df8d00fb17909a0a7786b56f967a79d4244d9
LiNk-NY <marcel.ramos@roswellpark.org>
2022-09-21 20:10:58 +0000

    update to include HDF5 folder

--- inst/scripts/make_docu.R
+++ inst/scripts/make_docu.R
@@ -40,3 +40,27 @@ write.csv(
     file = "~/gh/CyCIFData/inst/extdata/docuData/CyCIFData_v2.csv",
     row.names = FALSE
 )
+
+# version 3
+## REMOTE ONLY
+df <- data.frame(
+    DataProvider = "Laboratory of Systems Pharmacology at Harvard Medical School",
+    TaxonomyId = "9606",
+    Species = "Homo sapiens",
+    Title = paste0("Exemplar Microscopy Images of Tissues (EMIT) as ", c("ZARR", "HDF5")),
+    SourceUrl = "https://www.synapse.org/#!Synapse:syn22345748/wiki/",
+    SourceType = "TXT",
+    Location_Prefix = "https://mghp.osn.xsede.org/",
+    RDataPath = paste0("bir190004-bucket01/TMA11/", c("zarr/", "hdf5/")),
+    DataType = c("Zarr", "HDF5"),
+    DispatchClass = "FilePath",
+    RDataClass = "character",
+    SourceVersion = "1.0.0",
+    Maintainer  = "Ludwig Geistlinger <ludwig_geistlinger@hms.harvard.edu>",
+    stringsAsFactors = FALSE
+)
+write.csv(
+    df,
+    file = "~/gh/CyCIFData/inst/extdata/docuData/CyCIFData_v3.csv",
+    row.names = FALSE
+)
