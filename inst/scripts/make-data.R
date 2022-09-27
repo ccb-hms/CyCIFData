80648dd544b8e75a6e27a78d8440b0031ea93467
LiNk-NY <marcel.ramos@roswellpark.org>
2022-09-27 18:37:08 +0000

    updates to include remote Rds files

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -3,4 +3,11 @@ dat <- data.frame(
     bucket = "bir190004-bucket01/TMA11/zarr/"
 )
 
-write.dcf(dat, file = "~/data/CyCIFData/TMA11_osn.dcf")
+write.dcf(dat, file = "~/data/CyCIFData/TMA11_OSN_ZARR.dcf")
+
+dat <- data.frame(
+    hostname = "https://mghp.osn.xsede.org",
+    bucket = "bir190004-bucket01/TMA11/hdf5/"
+)
+
+write.dcf(dat, file = "~/data/CyCIFData/TMA11_OSN_H5.dcf")
--- inst/scripts/make_docu.R
+++ inst/scripts/make_docu.R
@@ -64,3 +64,34 @@ write.csv(
     file = "~/gh/CyCIFData/inst/extdata/docuData/CyCIFData_v3.csv",
     row.names = FALSE
 )
+
+# version 4
+## all REMOTE
+df <- data.frame(
+    DataProvider = "Laboratory of Systems Pharmacology at Harvard Medical School",
+    TaxonomyId = "9606",
+    Species = "Homo sapiens",
+    Title = c(
+        paste0("Exemplar Microscopy Images of Tissues (EMIT) as ", c("ZARR", "HDF5")),
+        rep("Associated Expression Data for EMIT", 4)
+    ),
+    SourceUrl = "https://www.synapse.org/#!Synapse:syn22345748/wiki/",
+    SourceType = c("TXT", "TXT", rep("Rds", 4)),
+    Location_Prefix = rep("https://mghp.osn.xsede.org/", 6),
+    RDataPath = paste0(
+        "bir190004-bucket01/TMA11/", c("zarr/", "hdf5/",
+        paste0("rds/",
+            c("coldata.rds", "exprs.rds", "rowdata.rds", "tissues.rds")))
+    ),
+    DataType = c("Zarr", "HDF5", rep("Expression", 4)),
+    DispatchClass = "FilePath",
+    RDataClass = "character",
+    SourceVersion = "1.0.0",
+    Maintainer  = "Ludwig Geistlinger <ludwig_geistlinger@hms.harvard.edu>",
+    stringsAsFactors = FALSE
+)
+write.csv(
+    df,
+    file = "~/gh/CyCIFData/inst/extdata/docuData/CyCIFData_v4.csv",
+    row.names = FALSE
+)
