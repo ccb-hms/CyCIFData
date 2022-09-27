d20a01829364b40063cd8b8b316a8be174f0200a
LiNk-NY <marcel.ramos@roswellpark.org>
2022-09-27 18:05:20 +0000

    use nameCol argument

--- R/utils.R
+++ R/utils.R
@@ -2,5 +2,5 @@
     function(fpath, otype = "H5I_DATASET", remote = TRUE, nameCol = "name")
 {
     l1 <- rhdf5::h5ls(fpath, recursive = FALSE, s3 = remote)
-    l1[l1$otype == otype, "name"]
+    l1[l1$otype == otype, nameCol]
 }
