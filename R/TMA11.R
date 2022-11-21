bf7dec5b5c59157b65ae1df34b05388398eac1ce
lgeistlinger <ludwig.geistlinger@gmail.com>
2022-11-21 15:16:47 -0500

    data loading by component

--- R/TMA11.R
+++ R/TMA11.R
@@ -22,26 +22,46 @@
 #' }
 #'
 #' @export
-TMA11 <- function(format = c("hdf5", "zarr")) {
+TMA11 <- function(data.component = c("expression", "images", "masks"), 
+                  format = c("hdf5", "zarr"))
+{
+    data.component <- match.arg(data.component)
+    if(data.component == "expression") .loadExpression()
+    else if(data.component == "images") .loadImages(format)
+    else .loadMasks()
+}
+
+.loadImages <- function(format = c("hdf5", "zarr"))
+{
     format <- match.arg(format)
     ## use for now until EH resource is live
-    zr <- ZarrRemote(
+    zr <- ZarrExperiment::ZarrRemote(
         endpoint = "https://mghp.osn.xsede.org/",
         bucket = paste0("bir190004-bucket01/TMA11/", format, "/")
     )
     if (identical(format, "zarr"))
         return(zr)
-    files <- files(zr)
+    files <- ZarrExperiment::files(zr)
     ex_files <- setNames(files, basename(files))
-    cytomapper::CytoImageList(
+    CytoImageList::CytoImageList(
         lapply(
             ex_files, function(file) {
                 HDF5Array::HDF5Array(
-                    H5File(file, s3 = TRUE), .get_h5_name(file)
+                    HDF5Array::H5File(file, s3 = TRUE), .get_h5_name(file)
                 )
             }
         ),
         on_disk = TRUE,
         h5FilesPath = HDF5Array::getHDF5DumpDir()
     )
+}
+
+.loadExpression <- function()
+{
+
+}
+
+.loadMasks <- function()
+{
+
 }
